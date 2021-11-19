6de37e361b6df0fc49eb9ba5c34201a695353914
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-19 19:18:58 +0100

    Source system, refactoring definition system, error system

--- aback.py
+++ aback.py
@@ -3,20 +3,21 @@ class AbackInterpreter:
     def __init__(self):
         self.stack = []
         self.words = {}
+        self.source = ''
 
     def interpret(self, code):
         lines = code.split('\n')
         line_index = 0
-        mode = 'normal'
         def_tree = []
         while line_index < len(lines):
             line = lines[line_index]
             words = line.split()
             word_index = 0
+            #print(f'{line_index}: {line}')
             while word_index < len(words):
                 # interpret words
                 word = words[word_index]
-                if mode == 'normal':
+                if len(def_tree) == 0:
                     if word == 'print':
                         print(self.stack.pop())
                     elif word == '+':
@@ -37,41 +38,43 @@ class AbackInterpreter:
                         self.stack.append(self.stack[-2])
                     elif word == 'rot':
                         self.stack[-1], self.stack[-2], self.stack[-3] = self.stack[-3], self.stack[-1], self.stack[-2]
-                    elif word in self.words:
-                        try:
-                            self.words[word](self)
-                        except IndexError:
-                            print('Not enough arguments for ' + word)
                     elif word.isdigit():
                         self.stack.append(int(word))
                     elif word.replace('.', '').isdigit():
                         self.stack.append(float(word))
                     elif word == '"':
-                        mode = 'string'
                         def_tree.append(['string', []])
-                    elif word[-1] == ':':
-                        mode = 'def'
-                        def_tree.append(['def', word[:-1], []])
+                    elif word[-1] == ':' and len(word) > 1:
+                        # checking if name is valid
+                        if not word[1].isdigit() and not word[:-1] in ('print', '+', '-', '*', '/', 'dup', 'drop', 'swap', 'over', 'rot', '"', 'flush', ':', ';;'):
+                            def_tree.append(['def', word[:-1], []])
+                        else:
+                            return False, self, ('InvalidNameError', f'Name {word[:-1]} may not be used.'), (line_index + 1, word_index + 1, line)
                     elif word == 'flush':
                         self.stack = []
+                    elif word == '':
+                        # empty word - do nothing
+                        pass
                     elif word in self.words:
-                        self.interpret(self.words[word])
-                elif mode == 'string':
+                        try:
+                            self.interpret(self.words[word])
+                        finally: pass
+                    else:
+                        return False, self, ('UnknownWordError', f'Word {word} was not found.'), (line_index + 1, word_index + 1, line)
+                elif def_tree[-1][0] == 'string':
                     if word == '"':
-                        mode = 'normal'
                         self.stack.append(" ".join(def_tree[-1][1]))
                         def_tree.pop()
                     elif word == '\\"':
                         def_tree[-1][1].append('"')
                     else:
                         def_tree[-1][1].append(word)
-                elif mode == 'def':
+                elif def_tree[-1][0] == 'def':
                     if word == ';;':
-                        mode = 'normal'
                         self.words[def_tree[-1][1]] = " ".join(def_tree[-1][2])
                         def_tree.pop()
                     else:
                         def_tree[-1][2].append(word)
                 word_index += 1
             line_index += 1
-        return True, self, None, mode # success, interpreter, error, mode
+        return True, self, None, (line_index + 1, word_index + 1, line) # success, interpreter, error, position
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from aback import AbackInterpreter
 from sys import argv
+from blessed import Terminal
 
 interpreter = AbackInterpreter()
+term = Terminal()
+
+def repr_error(source, line, word, name, line_string, error_details):
+    # fancy error printing
+    return f"""{term.bold + term.green}{source}{term.normal}({line}:{word}) {term.bold + term.red}{name}{term.normal}:
+    {term.blue}|{term.normal} {line_string}
+    {term.blue}|{term.normal} {error_details}"""
 
 if len(argv) > 1:
+    interpreter.source = argv[1]
     with open(argv[1]) as file:
         code = file.read()
-        success, intr, err, mode = interpreter.interpret(code)
+        success, intr, err, pos = interpreter.interpret(code)
         if not success:
-            print(err)
+            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1]))
 else:
+    interpreter.source = '<stdin>'
     while True:
         code = input('aback> ')
         if code == 'exit':
             break
-        success, intr, err, mode = interpreter.interpret(code)
+        success, intr, err, pos = interpreter.interpret(code)
         if not success:
-            print(err)
-        print(intr.stack, mode)
+            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1]))
+        print(intr.stack)
