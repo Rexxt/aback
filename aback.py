7ea534bba598108b9d639a4977e4842add984cc2
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-21 13:44:20 +0100

    buncha updates

--- aback.py
+++ aback.py
@@ -1,33 +1,12 @@
 # Aback, a FORTH-like language
-class AbackInterpreter:
+class AbackLanguage:
     def __init__(self):
         self.stack = []
         self.words = {}
         self.vars = {}
         self.source = ''
         self.version = '0.1.0'
-
-    def repr_data(self, data):
-        if data == True:
-            return 'true'
-        elif data == False:
-            return 'false'
-        elif data == None:
-            return 'null'
-        else:
-            return str(data)
-    
-    def repr_stack(self, stack):
-        return ', '.join([self.repr_data(data) for data in stack])
-    
-    def repr_vars(self, vars):
-        return ', '.join([f"{k}: {self.repr_data(vars[k])}" for k in vars])
-
-    def interpret(self, code):
-        lines = code.split('\n')
-        line_index = 0
-        def_tree = []
-        defined_words = ('print',
+        self.defined_words = ('print',
             '+',
             '-',
             '*', 
@@ -59,6 +38,124 @@ class AbackInterpreter:
             '$',
             'exit'
         )
+        self.processed_code = ''
+
+    def repr_data(self, data):
+        if data == True:
+            return 'true'
+        elif data == False:
+            return 'false'
+        elif data == None:
+            return 'null'
+        else:
+            return str(data)
+    
+    def repr_stack(self, stack):
+        return ', '.join([self.repr_data(data) for data in stack])
+    
+    def repr_vars(self, vars):
+        return ', '.join([f"{k}: {self.repr_data(vars[k])}" for k in vars])
+
+    """def pre_process(self, code):
+        # Check for errors, assign variables, predefine words
+        lines = code.split('\n')
+        line_index = 0
+        def_tree = []
+        processed_lines = []
+        temp_stack = []
+        while line_index < len(lines):
+            line = lines[line_index]
+            words = line.split()
+            word_index = 0
+            processed_words = []
+            while word_index < len(words):
+                # Preprocessor
+                word = words[word_index]
+                if len(def_tree) == 0: # In normal state
+                    if word.startswith("$"):
+                        # Variable preassignment
+                        if not word[1].isdigit() and not word[1:] in self.words and not word[:-1] in self.defined_words and word[1:].isalnum():
+                            self.vars[word[1:]] = temp_stack.pop()
+                        else:
+                            return False, self, ('InvalidNameError', f'Name \'{word[1:]}\' may not be used.'), (line_index + 1, word_index + 1, line), False
+                    elif word[-1] == ':' and len(word) > 1:
+                        # Word predefinition
+                        if not word[1].isdigit() and not word[:-1] in self.defined_words:
+                            def_tree.append(['def', word[:-1], []])
+                        else:
+                            return False, self, ('InvalidNameError', f'Name \'{word[:-1]}\' may not be used.'), (line_index + 1, word_index + 1, line), False
+                    elif word == 'include':
+                        # include mode
+                        def_tree.append(['include', ['']])
+                    elif word.isdigit():
+                        temp_stack.append(int(word))
+                    elif word.replace('.', '').isdigit():
+                        temp_stack.append(float(word))
+                    elif word == '"':
+                        def_tree.append(['string', []])
+                    elif word == '':
+                        # empty word - do nothing
+                        pass
+                    elif word in self.words:
+                        processed_words.append(word)
+                    elif word in self.vars:
+                        temp_stack.append(self.vars[word])
+                        processed_words.append(word)
+                    else:
+                        processed_words.append(word)
+                elif def_tree[-1][0] == 'def':
+                    if word == ';;':
+                        self.words[def_tree[-1][1]] = " ".join(def_tree[-1][2])
+                        def_tree.pop()
+                    else:
+                        def_tree[-1][2].append(word)
+                elif def_tree[-1][0] == 'string':
+                    if word == '"':
+                        self.stack.append(" ".join(def_tree[-1][1]))
+                        def_tree.pop()
+                    elif word == '\\"':
+                        def_tree[-1][1].append('"')
+                    elif word == '':
+                        def_tree[-1][1].append(' ')
+                    else:
+                        def_tree[-1][1].append(word)
+                elif def_tree[-1][0] == 'include':
+                    if word == ';;':
+                        for module in def_tree[-1][1]:
+                            mod = module + '.abk'
+                            try:
+                                with open(mod) as f:
+                                    success, intr, err, pos, do_exit = self.pre_process(f.read()) # Recursive preprocessing
+                                    if not success:
+                                        intr.source = module
+                                        return False, intr, err, pos, do_exit
+                                    if do_exit:
+                                        return True, self, None, (line_index + 1, word_index + 1, line), True
+                            except FileNotFoundError:
+                                return False, self, ('FileNotFoundError', f'File \'{mod}\' was not found.'), (line_index + 1, word_index + 1, line), False
+                        def_tree.pop()
+                    elif word == '&&':
+                        def_tree[-1][1].append('')
+                    else:
+                        def_tree[-1][1][-1] += word if word != '' else ' '
+                word_index += 1
+            processed_lines.append(" ".join(processed_words))
+            line_index += 1
+        if len(def_tree) > 0:
+            def_name_repr = ''
+            if def_tree[-1][0] == 'def':
+                def_name_repr = 'word'
+            else:
+                def_name_repr = def_tree[-1][0]
+            return False, self, ('EOFError', f'Unexpected end of file while defining {def_name_repr}. Please make sure all definitions are finished.'), (line_index + 1, word_index + 1, line), False
+        self.processed_code = '\n'.join(processed_lines)
+        return True, self, None, (line_index + 1, word_index + 1, line), False # success, interpreter, error, position, exit
+"""
+
+    def interpret(self, code):
+        lines = code.split('\n')
+        line_index = 0
+        def_tree = []
         while line_index < len(lines):
             line = lines[line_index]
             words = line.split()
@@ -67,7 +164,6 @@ class AbackInterpreter:
             while word_index < len(words):
                 # interpret words
                 word = words[word_index]
-                # debug
                 if len(def_tree) == 0:
                     if word == 'print':
                         print(self.repr_data(self.stack.pop()))
@@ -107,7 +203,7 @@ class AbackInterpreter:
                         def_tree.append(['string', []])
                     elif word[-1] == ':' and len(word) > 1:
                         # checking if name is valid
-                        if not word[1].isdigit() and not word[:-1] in defined_words:
+                        if not word[1].isdigit() and not word[:-1] in self.defined_words:
                             def_tree.append(['def', word[:-1], []])
                         else:
                             return False, self, ('InvalidNameError', f'Name \'{word[:-1]}\' may not be used.'), (line_index + 1, word_index + 1, line), False
@@ -128,7 +224,7 @@ class AbackInterpreter:
                         def_tree.append(['include', ['']])
                     elif word.startswith("$"):
                         #print(not word[1].isdigit(), not word[:-1] in self.words, not word[:-1] in defined_words, word[:-1].isalnum())
-                        if not word[1].isdigit() and not word[1:] in self.words and not word[:-1] in defined_words and word[1:].isalnum():
+                        if not word[1].isdigit() and not word[1:] in self.words and not word[:-1] in self.defined_words and word[1:].isalnum():
                             self.vars[word[1:]] = self.stack.pop()
                         else:
                             return False, self, ('InvalidNameError', f'Name \'{word[1:]}\' may not be used.'), (line_index + 1, word_index + 1, line), False
@@ -170,10 +266,12 @@ class AbackInterpreter:
                             mod = module + '.abk'
                             try:
                                 with open(mod) as f:
-                                    success, intr, err, pos = self.interpret(f.read())
+                                    success, intr, err, pos, do_exit = self.interpret(f.read())
                                     if not success:
                                         intr.source = module
-                                        return False, intr, err, pos
+                                        return False, intr, err, pos, do_exit
+                                    if do_exit:
+                                        return True, self, None, (line_index + 1, word_index + 1, line), True
                             except FileNotFoundError:
                                 return False, self, ('FileNotFoundError', f'File \'{mod}\' was not found.'), (line_index + 1, word_index + 1, line), False
                         def_tree.pop()
--- main.py
+++ main.py
@@ -1,32 +1,42 @@
-from aback import AbackInterpreter
+from aback import AbackLanguage
 from sys import argv
 from blessed import Terminal
 
-interpreter = AbackInterpreter()
+lang = AbackLanguage()
 term = Terminal()
 
-def repr_error(source, line, word, name, line_string, error_details):
+def repr_error(source, line, word, name, line_string, error_details, time): # time = "preprocess" or "run"
     # fancy error printing
-    return f"""{term.bold + term.green}{source}{term.normal}({line}:{word}) {term.bold + term.red}{name}{term.normal}:
+    return f"""{term.bold + term.green}{source}{term.normal}({line}:{word}) {term.bold + term.red}{name}{term.normal} [at {time}-time]:
     {term.blue}|{term.normal} {line_string}
     {term.blue}|{term.normal} {error_details}"""
 
 if len(argv) > 1:
-    interpreter.source = argv[1]
+    lang.source = argv[1]
     with open(argv[1]) as file:
         code = file.read()
-        success, intr, err, pos, do_exit = interpreter.interpret(code)
+        """preprocess_success, preprocess_intr, preprocess_err, preprocess_pos, preprocess_do_exit = lang.pre_process(code)
+        if not preprocess_success:
+            print(repr_error(preprocess_intr.source, preprocess_pos[0], preprocess_pos[1], preprocess_err[0], preprocess_pos[2], preprocess_err[1], "preprocess"))
+            # preprocessing failed, so we don't need to run the code
+            exit(1)"""
+        success, intr, err, pos, do_exit = lang.interpret(code)
         if not success:
-            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1]))
+            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1], "run"))
 else:
-    print(f"""Aback v{interpreter.version} shell
-Predefined variables: {interpreter.repr_vars(interpreter.vars)}""")
-    interpreter.source = '<stdin>'
+    print(f"""Aback v{lang.version} shell
+Predefined variables: {lang.repr_vars(lang.vars)}""")
+    lang.source = '<stdin>'
     while True:
         code = input('aback> ')
-        success, intr, err, pos, do_exit = interpreter.interpret(code)
+        """preprocess_success, preprocess_intr, preprocess_err, preprocess_pos, preprocess_do_exit = lang.pre_process(code)
+        if not preprocess_success:
+            print(repr_error(preprocess_intr.source, preprocess_pos[0], preprocess_pos[1], preprocess_err[0], preprocess_pos[2], preprocess_err[1], "preprocess"))
+            # preprocessing failed, so we don't need to run the code
+            continue"""
+        success, intr, err, pos, do_exit = lang.interpret(code)
         if not success:
-            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1]))
+            print(repr_error(intr.source, pos[0], pos[1], err[0], pos[2], err[1], "run"))
         print(term.orange + term.bold + '[' + intr.repr_stack(intr.stack) + ']' + term.normal)
         if do_exit:
             break
